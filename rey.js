c80d20acf97bf1f90cbf5b099427b7f8c2d7fa97
mfaiqrasis <faiqrasis1234@gmai.com>
2025-07-02 16:11:35 +0700

    jawa

--- rey.js
+++ rey.js
@@ -1,42 +1,18 @@
-const startEl = document.getElementById("start");
-const stopEl = document.getElementById("stop");
-const resetEl = document.getElementById("reset");
-const timerEl = document.getElementById("timer");
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
 
-let interval;
-let timeLeft = 1500;
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("en", {
+  month: "long",
+});
 
-function updateTimer() {
-  let minutes = Math.floor(timeLeft / 60);
-  let seconds = timeLeft % 60;
-  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
 
-  timerEl.innerHTML = formattedTime;
-}
+dayNumEl.innerText = date.getDate();
 
-function startTimer() {
-  interval = setInterval(() => {
-    timeLeft--;
-    updateTimer();
-    if (timeLeft === 0) {
-      clearInterval(interval);
-      alert("Time's up!");
-      timeLeft = 1500;
-      updateTimer();
-    }
-  }, 1000);
-}
-function stopTimer() {
-  clearInterval(interval);
-}
-function resetTimer() {
-  clearInterval(interval);
-  timeLeft = 1500;
-  updateTimer();
-}
-
-startEl.addEventListener("click", startTimer);
-stopEl.addEventListener("click", stopTimer);
-resetEl.addEventListener("click", resetTimer);
+yearEl.innerText = date.getFullYear();
